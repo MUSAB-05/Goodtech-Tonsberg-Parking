622f6bdb2ee2cb0334b58b170623e93fd4a2e27b
MUSAB-05 <mustafa.sabonchi@gmail.com>
2026-09-03 14:59:30 +0200

    Use dedicated claimed MantleDB namespace

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ export const APP_CONFIG = {
   timezone: 'Europe/Oslo',
   pollMs: 1500,
   mantleBaseUrl: 'https://mantledb.sh/v2',
-  mantleNamespace: 'gt-parking-musab-20260903-v3'
+  mantleNamespace: 'gt-parking-musab-20260903-v5',
+  mantleKey: '64f4a0e4a399023c0d94df7de23d44f1fb72a8f01d6e2bee73051a049e935675'
 };
